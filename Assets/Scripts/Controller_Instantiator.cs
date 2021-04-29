746e932c05cfedc1f00cd102f12814c75b3cc864
Lázaro <lazaroalberto1@gmail.com>
2021-04-29 01:13:13 -0300

    Comentarios de los enemigos

--- Assets/Scripts/Controller_Instantiator.cs
+++ Assets/Scripts/Controller_Instantiator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Controller_Instantiator : MonoBehaviour
 {
+    //I will need this to make the enemies work. Is important to add a list, so the game can choose wich one to spawn.
     public List<GameObject> enemies;
     public GameObject instantiatePos;
     public float timer = 7;
@@ -11,6 +12,7 @@ public class Controller_Instantiator : MonoBehaviour
 
     void Update()
     {
+        //Every tick this functions are called.
         timer -= Time.deltaTime;
         SpawnEnemies();
         ChangeVelocity();
@@ -18,6 +20,7 @@ public class Controller_Instantiator : MonoBehaviour
 
     private void ChangeVelocity()
     {
+        //It makes the enemies faster
         time += Time.deltaTime;
         if (time > multiplier)
         {
@@ -27,12 +30,13 @@ public class Controller_Instantiator : MonoBehaviour
 
     private void SpawnEnemies()
     {
-        if (timer <= 0)
+        if (timer <= 0) //decides when the enemies should spawn. I don't want the entire screen to be full of enemies.
         {
-            float offsetX = instantiatePos.transform.position.x;
-            int rnd = UnityEngine.Random.Range(0, enemies.Count);
+            float offsetX = instantiatePos.transform.position.x; //enemies spawn point
+            int rnd = UnityEngine.Random.Range(0, enemies.Count); //chooses a random enemy from the list
             for (int i = 0; i < 5; i++)
             {
+                //This for is for spawning a centain amount of enemies. 
                 offsetX = offsetX + 4;
                 Vector3 transform = new Vector3(offsetX, instantiatePos.transform.position.y, instantiatePos.transform.position.z);
                 Instantiate(enemies[rnd], transform, Quaternion.identity);
