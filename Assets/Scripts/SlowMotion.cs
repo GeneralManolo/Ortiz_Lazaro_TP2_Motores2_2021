f5189071f159817f40faa4db01df2ddf225ad7e6
Lázaro <lazaroalberto1@gmail.com>
2021-05-06 02:06:31 -0300

    Añadido el slow motion

--- Assets/Scripts/SlowMotion.cs
+++ Assets/Scripts/SlowMotion.cs
@@ -1,16 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SlowMotion : MonoBehaviour
 {
-   
-    void Start()
+    //allows me to configure how slowmotion will occur.
+    public float slowDownFactor = 0.05f; //how much is the slow down
+    public float slowDownLegth = 3f;
+
+    void activateSlowMotion()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Time.timeScale = slowDownFactor;
+        }
+
     }
-    void Update()
-    {
-        
-    }
+
 }
